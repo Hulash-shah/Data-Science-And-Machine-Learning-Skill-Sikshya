b893c3b0d1a7bcd0997f13929a88691c4f81f959
buny10 <shahhulash@gmail.com>
2026-08-12 00:25:15 +0545

    learnt about different funtions in python and practiced related question

--- practice/practice_day5.py
+++ practice/practice_day5.py
@@ -1,4 +1,128 @@
 
 # Funtions in python
+   
+   
+# #function call
+# calc_cgt(20000)
+
+# # funtions helps to reduce redundancy and maked code more usable .same logic can be used for multiple same type operations.
 
 
+
+# # types of funtions
+
+# # Module funtions => collection of related funtion , related classes , related variables
+
+# # for example math is a module function
+
+# import math 
+# from math import sqrt,  log2
+
+# print(dir(math))
+
+# print(sqrt(224))
+# print(log2(16))
+
+
+# import random
+
+# print(random.random()) # returns random numbers between o and 1 excluding 1
+
+# print(random.randint(1, 100))  # returns random numbers betweeen 1 and 100
+
+
+# # Practice exercise 6
+
+
+# def WAF(num):
+#     if num % 2 == 0:
+#         print("Number is even")
+        
+    
+#     else:
+#         print("number is odd")
+        
+# WAF(5)
+
+
+
+# def check(word: str) -> str:
+#     count = 0
+    
+#     for i in word:
+#         if i == 'a' or i== 'e' or i == 'i' or i == 'o' or i == 'u':
+#          count += 1
+
+#     return count
+        
+# print(check("vowel"))
+
+
+# def check_prime(num : int) -> int:
+#     if num > 1 and num % num == 0 and num % 1 == 0 and num % 2 != 0:
+#         print("number is prime")
+    
+#     else:
+#       print("number is not prime")
+        
+# check_prime(5)
+# check_prime(6)
+
+
+
+# def average(numbers: list[int]) -> float:
+#     return sum(numbers) / len(numbers)
+
+# print(average([2,3,4,5,6,7]))
+
+
+# def sum(a,b):
+#     return a + b
+# print(sum(2,4))
+
+
+
+
+
+# def calc_cgt(price):  # values we define in fuction are called paramenters . Here price is parameters
+#    return price * price * 0.10
+
+# print(calc_cgt(20000))  # values we pass in fuction call are called arguments
+
+# def calc_cgt(price):
+#    new_price = price * price * 0.10
+#    print(new_price)
+
+
+
+# creating a guessing game 
+import random
+
+def play_game():
+    lucky_number = random.randint(1, 100)
+    
+   
+    
+    while True:
+        user_number = int(input("Enter your guess"))
+        if user_number == lucky_number:
+          print ("Your Guess is Correct : You won the Game!!")
+          break
+    
+        elif user_number < lucky_number :
+          print("Your guess is too low")
+        
+    
+        elif user_number > lucky_number:
+          print("Your guess is too high")
+    
+    print("Thankyou for playing")
+play_game()
+    
+
+
+# naming conventions
+
+# camel case  #   myNameIs
+# pascal case  # MyNameIs
+# snake_case   # my_name_is
