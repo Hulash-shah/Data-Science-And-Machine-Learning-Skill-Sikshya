5418887dc32b4c1c27a4ae5ec99d260bf2ee45cb
buny10 <shahhulash@gmail.com>
2026-08-24 08:55:15 +0545

    update day 19 discussed assignment of day 18 and learnt basics of numpy

---
+++ Skill_Sikshya/Day19/Numpy.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+# print(np.__version__)
+
+
+# arr = [
+#     [1,2,3],
+#     [3,4,5],
+#     [4,5,6]
+
+# ]
+
+# arr2 = np.array(arr)
+
+# print(arr2)
+
+
+# arr3 = [
+    
+#     [
+#         [1,2,3],
+#         [3,4,5],
+#         [4,5,6]
+#     ],
+      
+#     [
+#         [1,2,3],
+#         [3,4,5],
+#         [4,5,6]
+#     ],
+
+# ]
+# arr4 = np.array(arr3)
+
+# print(arr4)
+# print(arr4.shape)
+# print(arr4.ndim)
+
+# zero = np.zeros(shape = (2, 3,6), dtype = int)
+# print(zero)
+
+# one = np.ones(shape = (2, 3,3), dtype = int)
+# print(one)
+
+# onee = np.ones(shape = (5, 2, 3,3), dtype = int)
+# print(onee)
+
+# full = np.full(shape = (3,4), fill_value = 6)
+# print(full)
+
+# range = np.arange(1,90,5)
+# print(range)
+
+# linspace = np.linspace((0,1,2), (30,20,60) , num = 13, axis = 0)
+# print(linspace)
+
+
+# identity = np.identity(200)\
+    
+# print(identity)
+
+
+# eye = np.eye(200)\
+    
+# print(eye)
+
+
+random = np.random.randint(0,50, size = (3,2,4))
+
+print(random)
